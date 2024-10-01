e08ec729a38d61dc1c774eeaff37e658f526072f
unknown <mtgmirage@protonmail.com>
2024-10-01 11:37:18 -0500

    refactored parkAreasHTML

--- src/parkAreasHTML.js
+++ src/parkAreasHTML.js
@@ -1,22 +1,49 @@
 // Import getAreas() from database
-import { getAreas } from "./database.js";
+import { getAreas, getAreaServices, getServices } from "./database.js";
 
 // Invoke areas = getAreas() and store its value
 const areas = getAreas()
+const areaServices = getAreaServices()
+const services = getServices()
 
 // Define and export the buildAreasHTML() 
 export const buildAreasHTML = () => {
-// Define HTML variable = opening <ul> tag
-    let areasHTML = "<ul>"
+// Define HTML variable = ""
+    let areasHTML = ""
     // Create the for...of loop to iterate the area of areas
     for (const area of areas) {
-        areasHTML += `<li data-id="${area.id}"
-            data-name="${area.name}"
-            >${area.name}
-        </li>`
+        // Create opening section and list tags + area name header"
+        areasHTML += `
+            <section>
+                <div data-id="${area.id}"
+                data-name="${area.name}"
+                >${area.name}
+                </div>
+                <ul>
+        `
+
+/*      for (const areaService of areaServices) {
+            if (areaService.areaId === area.id) {
+                for (const service of services) {
+                    if (areaService.serviceId === service.id) {
+                        areasHTML += `<li>${service.name}</li>`
+                    }
+                }   
+            } */      
+
+        // iterate over areaServices to populate the list of services for each area
+        for (const areaService of areaServices) {
+            if (areaService.areaId === area.id) {
+                const foundElement = services.find((service) => areaService.serviceId === service.id)
+                    areasHTML += `<li>${foundElement.name}</li>`
+            }
+        }
+        // Close HTML list and section tags
+        areasHTML += `
+                </ul>
+            </section>
+        `
     }
-    // Close HTML </ul> tag
-    areasHTML += "</ul>"
     // Return HTML string
     return areasHTML
 }
