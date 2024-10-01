12a257ed5118c14f5377e660dbe3402d248c6f30
Jayden Palmer <jayalexpalmer@gmail.com>
2024-10-01 11:39:51 -0500

    mistakes were made... parkinfo.js 1.2

--- scripts/parkInfo.js
+++ scripts/parkInfo.js
@@ -9,7 +9,7 @@ export const renderHeader = () => {
     <header>
         <div class="header-grouping">
             <img src="${park.logo}" alt="${park.name} logo" class="logo"/>
-            <h1>${park.name}</h1>
+            <h1 id="park-name">${park.name}</h1>
         </div>
     </header>
     `;
@@ -21,13 +21,14 @@ export const renderFooter = () => {
     return `
     <footer>
         <div class="footer-grouping">
-            <p>CONTACT US!</p>
-            <p>Phone: <a href="call:${park.phoneNumber}">${park.phoneNumber}</a></p>
-            <p>Email: <a href="mail:${park.email}">${park.email}</a></p>
-            <p>Address: ${park.address}</p>
+            <p id="contact">CONTACT US! </p>
+            <p id="phone">Phone: <a href="call:${park.phoneNumber}">${park.phoneNumber}</a></p>
+            <p id="email">Email: <a href="mail:${park.email}">${park.email}</a></p>
+            <p id="adress">Address: ${park.address}</p>
         </div>
         <div class="copyright">
             <p>${park.name}. All rights reserved.</p>
+        </div>
     </footer>
     `;
 }
